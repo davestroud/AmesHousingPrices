0dc183b477cbe5c43e4d47bd099e66dd88d641c0
David Stroud <david@greymatter-analytics.com>
2018-02-01 04:04:32 -0600

    Loaded necessary libraries. Add comments to README file

--- Baseline.R
+++ Baseline.R
@@ -1,5 +1,19 @@
+# Borrowed from Kaggle competition below to serve as a baseline  
+# https://www.kaggle.com/tannercarbonati/detailed-data-analysis-ensemble-modeling
+
+
 library(ggplot2) # Data visualization
+library(stringr) # Extracting string patterns
+library(glmnet) # Ridge, LASSO, and elastinet
+library(xgboost)
+library(randomForest)
+library(Metrics) # rmse
+library(dplyr)
+library(caret) # One hot encoding
+library(scales) # Plotting $$
+library(e1071) # Skewness
+library(corrplot) # Correlation plot
 library(readr) # To read csv function
 
 train <- read_csv("train.csv")
-head(train)
+test <- read_csv("test.csv")
