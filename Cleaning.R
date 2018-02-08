aa2af5ed50b419a5b7a4305bbd1f197c5701ba08
David Stroud <david@greymatter-analytics.com>
2018-02-07 22:15:10 -0600

    helper function for plotting categorical data

--- Cleaning.R
+++ Cleaning.R
@@ -27,3 +27,25 @@ na.cols <- which(colSums(is.na(df.combined)) > 0)
 sort(colSums(sapply(df.combined[na.cols], is.na)), decreasing = TRUE)
 
 paste('There are', length(na.cols), 'columns with missing values')
+
+
+# helper function for plotting categoric data for easier data visualization
+plot.categoric <- function(cols, df){
+  for (col in cols) {
+    order.cols <- names(sort(table(df.combined[,col]), decreasing = TRUE))
+    
+    num.plot <- qplot(df[,col]) +
+      geom_bar(fill = 'cornflowerblue') +
+      geom_text(aes(label = ..count..), stat='count', vjust=-0.5) +
+      theme_minimal() +
+      scale_y_continuous(limits = c(0,max(table(df[,col]))*1.1)) +
+      scale_x_discrete(limits = order.cols) +
+      xlab(col) +
+      theme(axis.text.x = element_text(angle = 30, size=12))
+    
+    print(num.plot)
+  }
+}
+
+
+df.combined[(df.combined$PoolArea > 0) & is.na(df.combined$PoolQC),c('PoolQC','PoolArea')]
